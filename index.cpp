1d1e4f458ad041549fbe3068c97c1377fbb98a30
Aram <aram.gumrikyan.01@gmail.com>
2022-12-13 00:22:08 +0400

    SP-2: use classes for crypto algorythms

--- index.cpp
+++ index.cpp
@@ -1,171 +1,33 @@
 #include "iostream"
-#include "RowColumnCrypto.h"
+#include "./src/RowColumnCrypto.h"
+#include "./src/TemplateCrypto.h"
+#include "./src/RleCrypto.h"
+#include "./src/Test.h"
+
 using namespace std;
-
-int greatestCommonDivisor(int num1, int num2)
-{
-  for (int i = min(num1, num2); i >= 2; i--)
-  {
-    if ((num1 % i == 0) && (num2 % i == 0))
-    {
-      return i;
-    }
-  }
-  return 1;
-}
-
-int smallestCommonMultiple(int num1, int num2)
-{
-  const int gcd = greatestCommonDivisor(num1, num2);
-  const int mod = abs(num1 * num2);
-  return mod / gcd;
-}
-
-int secondExercise(int numbers[], int numbersLength)
-{
-  const int possibleGreatestNumber = numbersLength + 1;
-
-  int realSum = 0, fakeSum = 0;
-
-  for (int i = 1; i <= numbersLength; i++)
-  {
-    realSum += i;
-
-    const int tempNumber = numbers[i - 1];
-
-    if (tempNumber == possibleGreatestNumber)
-    {
-      continue;
-    }
-
-    fakeSum += tempNumber;
-  }
-
-  if (realSum == fakeSum)
-  {
-    return possibleGreatestNumber;
-  }
-
-  return realSum - fakeSum;
-}
-
-string en = "AaBbCc1234";
-string de = "cCAaBb2143";
-
-string encodeShablon(string text)
-{
-  for (int i = 0; i <= text.length(); i++)
-  {
-    for (int j = 0; j <= en.length(); j++)
-    {
-      if (text[i] == en[j])
-      {
-        text[i] = de[j];
-        break;
-      }
-    }
-  }
-  return text;
-}
-string decodeShablon(string text)
-{
-  for (int i = 0; i <= text.length(); i++)
-  {
-    for (int j = 0; j <= en.length(); j++)
-    {
-      if (text[i] == de[j])
-      {
-        text[i] = en[j];
-        break;
-      }
-    }
-  }
-  return text;
-}
-
-string RLE_Algorithm(string str)
-{
-  string result = "";
-  int n = str.length();
-  for (int i = 0; i < n; i++)
-  {
-
-    int count = 1;
-    while (i < n - 1 && str[i] == str[i + 1])
-    {
-      count++;
-      i++;
-    }
-    result += (str[i] + to_string(count));
-  }
-
-  return result;
-}
-
-void test1()
-{
-  const int res1 = greatestCommonDivisor(6936, 1200);
-  const int res2 = greatestCommonDivisor(192, 348);
-  const int res3 = smallestCommonMultiple(6936, 1200);
-  const int res4 = smallestCommonMultiple(192, 348);
-
-  if (res1 != 24 || res2 != 12 || res3 != 346800 || res4 != 5568)
-  {
-    cout << "1: Test fails" << endl;
-    return;
-  }
-
-  cout << "1: Test success" << endl;
-}
-
-void test2()
-{
-  int case1[] = {2, 3, 4, 5, 6};
-  int case2[] = {1, 2, 4, 5, 6};
-  int case3[] = {1, 2, 3, 4, 5, 6, 7, 9};
-  int case4[] = {1, 2, 3, 4, 5, 6, 7, 8};
-  const int res1 = secondExercise(case1, 5);
-  const int res2 = secondExercise(case2, 5);
-  const int res3 = secondExercise(case3, 8);
-  const int res4 = secondExercise(case4, 8);
-
-  if (res1 != 1 || res2 != 3 || res3 != 8 || res4 != 9)
-  {
-    cout << "2: Test fails" << endl;
-    return;
-  }
-
-  cout << "2: Test success" << endl;
-}
 
 int main()
 {
-  const u_int key = 3;
-  RowColumnCrypto Crypto;
-  const string encryptedData = Crypto.encrypt("123456789ABCDEFGHI", key);
-  const string decryptedData = Crypto.decrypt(encryptedData, key);
-  cout << encryptedData << endl;
-  cout << decryptedData << endl;
-  cout << encodeShablon("abc42A") << endl;
-  cout << decodeShablon("Cab31c") << endl;
-  cout << RLE_Algorithm("aaabaaabbba") << endl;
-
   test1();
   test2();
-  /*
-  123
-  456
-  789
-  ABC
-  DEF
-  GHI
-  ====
-  147
-  258
-  369
-  ADG
-  BEH
-  CFI
-  */
+
+  const u_int key = 3;
+  RowColumnCrypto rowColumnCrypto;
+  const string encodedData = rowColumnCrypto.encode("123456789ABCDEFGHI", key);
+  const string decodedData = rowColumnCrypto.decode(encodedData, key);
+  cout << encodedData << endl;
+  cout << decodedData << endl;
+
+  TemplateCryptoKey templateCryptoKey;
+  templateCryptoKey.initial = "AaBbCc1234";
+  templateCryptoKey.toChange = "cCAaBb2143";
+  TemplateCrypto templateCrypto;
+  cout << templateCrypto.encode("abc42A", templateCryptoKey) << endl;
+  cout << templateCrypto.decode("Cab31c", templateCryptoKey) << endl;
+
+  RleCrypto rleCrypto;
+  cout << rleCrypto.encode("aaabaaabbba") << endl;
+  cout << rleCrypto.decode("a3b1a3b3a1") << endl;
+
   return 0;
 }
